6139039dfa0cdb8d66312933e0c56a95b26caffd
Daniel Constantinidis <dancon98@hotmail.com>
2024-07-01 18:07:02 +1000

    add airline status bar

--- daniel-laptop/nvim/lua/daniel/packer.lua
+++ daniel-laptop/nvim/lua/daniel/packer.lua
@@ -3,41 +3,41 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
+-- Packer init
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+-- Colors! 
+use({
+  'catppuccin/nvim',
+  as = 'catppuccin',
+  config = function()
+          vim.cmd('colorscheme catppuccin')
+  end
+})
+
+
+-- File navigation -- 
+use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+}
 use {
   'nvim-telescope/telescope.nvim', tag = '0.1.4',
 -- or                            , branch = '0.1.x',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
 
-  use({
-	  'catppuccin/nvim',
-	  as = 'catppuccin',
-	  config = function()
-	          vim.cmd('colorscheme catppuccin')
-	  end
-  })
 
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use('nvim-treesitter/playground')
-use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-use {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    requires = { {"nvim-lua/plenary.nvim"} }
-}
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
+-- LSP Support
 use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v3.x',
   requires = {
     --- Uncomment these if you want to manage LSP servers from neovim
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
     -- LSP Support
     {'neovim/nvim-lspconfig'},
@@ -47,19 +47,30 @@ use {
     {'L3MON4D3/LuaSnip'},
   }
 }
+
 use {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
 }
 
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use('nvim-treesitter/playground')
+
+-- Status bar --
+use { 'vim-airline/vim-airline' }
+use { 'vim-airline/vim-airline-themes' }
+
+-- AI -- 
+-- use('Exafunction/codeium.vim')
+
+
+-- Misc -- 
+use('mbbill/undotree')
+use('tpope/vim-fugitive')
 use({
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
 })
-
-use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-
-
-
+use "nvim-lua/plenary.nvim"
 end)
--- daniel-laptop/nvim/plugin/packer_compiled.lua
+++ daniel-laptop/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/daniel/.cache/nvim/packer_hererocks/2.1.1699801871/share/lua/5.1/?.lua;/Users/daniel/.cache/nvim/packer_hererocks/2.1.1699801871/share/lua/5.1/?/init.lua;/Users/daniel/.cache/nvim/packer_hererocks/2.1.1699801871/lib/luarocks/rocks-5.1/?.lua;/Users/daniel/.cache/nvim/packer_hererocks/2.1.1699801871/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/daniel/.cache/nvim/packer_hererocks/2.1.1699801871/lib/lua/5.1/?.so"
+local package_path_str = "/Users/daniel/.cache/nvim/packer_hererocks/2.1.1716656478/share/lua/5.1/?.lua;/Users/daniel/.cache/nvim/packer_hererocks/2.1.1716656478/share/lua/5.1/?/init.lua;/Users/daniel/.cache/nvim/packer_hererocks/2.1.1716656478/lib/luarocks/rocks-5.1/?.lua;/Users/daniel/.cache/nvim/packer_hererocks/2.1.1716656478/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/daniel/.cache/nvim/packer_hererocks/2.1.1716656478/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -154,6 +154,16 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/daniel/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
+  },
+  ["vim-airline"] = {
+    loaded = true,
+    path = "/Users/daniel/.local/share/nvim/site/pack/packer/start/vim-airline",
+    url = "https://github.com/vim-airline/vim-airline"
+  },
+  ["vim-airline-themes"] = {
+    loaded = true,
+    path = "/Users/daniel/.local/share/nvim/site/pack/packer/start/vim-airline-themes",
+    url = "https://github.com/vim-airline/vim-airline-themes"
   },
   ["vim-fugitive"] = {
     loaded = true,
